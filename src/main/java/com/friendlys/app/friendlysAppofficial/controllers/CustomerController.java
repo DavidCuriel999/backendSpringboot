99628baa77dbf34528151ab4759d331791bdb194
DavidCuriel999 <133292821+DavidCuriel999@users.noreply.github.com>
2025-10-06 23:46:29 -0600

    Update

--- src/main/java/com/friendlys/app/friendlysAppofficial/controllers/CustomerController.java
+++ src/main/java/com/friendlys/app/friendlysAppofficial/controllers/CustomerController.java
@@ -1,6 +1,8 @@
 package com.friendlys.app.friendlysAppofficial.controllers;
 
 import com.friendlys.app.friendlysAppofficial.domain.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -19,34 +21,36 @@ public class CustomerController {
             new Customer(769, "Laura Sanchez", "Lauras", "secreto789"),
             new Customer(234,"Carlos Martinez", "carlosm", "password234")
     ));
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public ResponseEntity<List<Customer>> getCustomers() {
 
-    @RequestMapping(method = RequestMethod.GET)
-    //@GetMapping
-    public List<Customer> getCustomers() {
-
-        return customers;
+        return ResponseEntity.ok(customers);
+        //return customers;
     }
 
-    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
-    //@GetMapping("/{username}")
-    public Customer getCliente(@PathVariable String username){
+    //@RequestMapping(value = "/{username}", method = RequestMethod.GET)
+    @GetMapping("/{username}")
+    public ResponseEntity<?> getCliente(@PathVariable String username){
         for (Customer c : customers) {
             if(c.getUsername().equalsIgnoreCase(username)) {
-                return c;
+                return ResponseEntity.ok(c);
+                //return c;
             }
         }
-        return null; // Mala practica
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no encotrado con username: "+ username);
+       // return null; // Mala practica
 
     }
-    @RequestMapping(method = RequestMethod.POST)
-    //@PostMapping
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public Customer postCliente(@RequestBody Customer customer){
         customers.add(customer);
         return customer;
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
-    //@PutMapping
+    //@RequestMapping(method = RequestMethod.PUT)
+    @PutMapping
     public Customer putCliente(@RequestBody Customer customer) {
         for (Customer c : customers) {
             if (c.getID() == customer.getID()) {
@@ -59,8 +63,8 @@ public class CustomerController {
         return null; //Es mala practica se utiliza Manejo de excepciones
     }
 
-    @RequestMapping(value= "/{id}", method =RequestMethod.DELETE)
-    //@DeleteMapping("/{id}")
+    //@RequestMapping(value= "/{id}", method =RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
     public Customer deleteCliente(@PathVariable int id){ ;
         for (Customer c : customers) {
             if (c.getID() == id) {
@@ -72,8 +76,8 @@ public class CustomerController {
         return null;
     }
 
-    @RequestMapping(method = RequestMethod.PATCH)
-    //@PatchMapping
+    //@RequestMapping(method = RequestMethod.PATCH)
+    @PatchMapping
     public Customer patchCliente(@RequestBody Customer customer){
         for(Customer c : customers){
             if(c.getID() == customer.getID()){
